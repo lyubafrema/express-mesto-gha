49fe0a6dc40564c55ecd50a59f3dc0cc652a941f
Lyubov Efremova <112565528+lyubafrema@users.noreply.github.com>
2023-04-12 22:23:02 +0300

    Delete routes directory

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const usersRouter = require('./users');
-const cardsRouter = require('./cards');
-
-const router = require('express').Router();
-
-router.use('/users', usersRouter);    // общий роутер, рапределяет запрос на users
-router.use('/cards', cardsRouter);   // общий роутер, рапределяет запрос на cards
-
-module.exports = router;
